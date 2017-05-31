82ebaba64c0cfadb67c9013343aca7e9bb2e6e39
Yann DERYCKE <yderycke@adneom.com>
2017-05-31 21:10:02 +0200

    feat(markDown): add module markDown, live and render

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'DataCamp';
+  text:string;
+  live: false;
+  textData = `## Markdown content data`;
+  textDataTa: string
+  constructor() {
+    this.textData = localStorage.getItem('markDown')
+    this.textDataTa = localStorage.getItem('markDown')
+  }
+
+  public store(e) {
+    localStorage.setItem('markDown', e.target.value);
+    if (this.live) {
+      this.textData = e.target.value;
+    } else {
+      this.text = e.target.value;
+    }
+    
+  }
+
+  public getMarkDown() {
+    this.textData = this.text;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { MarkdownModule } from 'angular2-markdown';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MarkdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
